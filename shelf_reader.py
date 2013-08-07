72ebb7eced4ff3e71c9e20490a5f2c40811b9283
asmacdo <asmacdo@gmail.com>
2013-08-07 17:02:09 -0400

    updated to more standard docstring

--- shelf_reader.py
+++ shelf_reader.py
@@ -2,25 +2,54 @@ import csv
 import re
 from sys import argv
 
-# File name of csc file containing barcodes and call numbers.
-filename = "numbers.csv"
+FILE_NAME = "numbers.csv"
+"""
+name of csv file with barcodes and call numbers.
+"""
 
-# Creates a dictionary from a csv file containing the catalog of barcodes in 
-# the first column and call numbers in the second column. Creates a dictionary 
-# from the first two columns of the csv file.
-with open(filename, mode='r') as infile:
-    reader = csv.reader(infile)
-    call_number_lookup = {rows[0]:rows[1] for rows in reader}
+def create_dictionary(filename):
+    """
+    Creates a dictionary from a csv file containing the catalog of barcodes in 
+    the first column and call numbers in the second column. Creates a dictionary 
+    from the first two columns of the csv file.
+    
+    :param filename:    name of csv file with barcodes and call numbers
+    
+    :returns:            dictionary of barcodes and call numbers       
+    """
+    
+    with open(filename, mode='r') as infile:   
+        reader = csv.reader(infile)
+        call_number_lookup = {rows[0]:rows[1] for rows in reader}
+        return call_number_lookup
 	
-# Prompts the user for a barcode and returns the appropriate call number.
-def get_call_number():
-	barcode = raw_input("Barcode>>>")
-	call_number = call_number_lookup[barcode]
-	return call_number
+def get_call_number(call_number_lookup):
+    """
+    Prompts the user for a barcode and returns the appropriate call number. If
+    the user inputs a barcode that is not in the dictionary, the user is 
+    prompted again.
+    
+    :param call_number_lookup:  dictionary of barcodes and call numbers
+    
+    :returns:                   call number that matches user input barcode
+    """
+    
+    while True:
+        barcode = raw_input("Barcode>>>")
+        try: 
+            return call_number_lookup[barcode]
+        except:
+            print "Invalid barcode, try again"
 
-# Checks the type of character of the input. Returns 0 for letter, 1 for 
-# number, or 2 for space. 
 def check_type(c):
+    """
+    Checks the type of character of the input.
+    
+    :param c:   character to check
+    
+    :returns:    0 if letter, 2 if space, or 1 if number
+    """
+    
 	letter_check = re.compile('[a-zA-Z]')
 	if letter_check.match(c): # True if c is a letter
 		return 0 #letter
@@ -29,9 +58,18 @@ def check_type(c):
 	else:
 		return 1 # number
 		
-# Breaks a call number into tokens and removes spaces. Returns a list of the
-# tokens.
 def break_call_number(call_number):
+    """
+    Breaks a call number into tokens by grouping like characters (ie letters
+    with letters) and removing spaces. Also ensures that whole numbers should
+    be treated as whole numbers and decimals treated as decimals.
+    
+    :param call_number: the call number to be broken
+    
+    :returns broken:    a comma seperated list of tokens containing like
+                        characters.
+    """
+    
 	broken = []
 	next = call_number[0]
 	
@@ -51,11 +89,17 @@ def break_call_number(call_number):
 			next = d				
 	return broken
 	
-
-# Compares two tokens of the call number and determines if they are sorted
-# correctly. 
-# Returns True or False.
 def is_sorted(a, b):
+    """
+    Compares two tokens and determines if they are sorted correctly. 
+    
+    :param a:   token from the first call number
+    
+    :param b:   token from the second call number
+    
+    :returns:   True if the tokens are in the correct order, False otherwise
+    """
+    
 	if (check_type(a) == 1): # Number
 		return (a <= b)
 	else: # Letters
@@ -64,8 +108,17 @@ def is_sorted(a, b):
 		list.sort()
 		return (list == not_sorted)	
 
-# Compares two call numbers and determines if they are in the correct order.
 def is_correct(call_a, call_b):
+    """
+    Compares two call numbers and determines if they are in the correct order.
+    
+    :param call_a:  first call number
+    
+    :param call_b:  second call number
+    
+    :returns:       True if in the correct order, False otherwise
+    """
+    
 	for i in range(len(call_a)):
 		if call_a[i] != call_b[i]: 
 			if not is_sorted(call_a[i], call_b[i]):
@@ -73,32 +126,58 @@ def is_correct(call_a, call_b):
 			return True
 	return True
 	
-# Reaction to the correct order	
-def correct():
+def correct(call_a, call_b):
+    """
+    Informs the user that the order is correct.
+    
+    :param call_a:  first call number - not used at this time but could be
+                    important in later versions.
+                    
+    :param call_b:  second call number - not used at this time but could be
+                    important in later versions.
+    """
+    
 	print "_________________________________"
 	print "|                                |"
 	print "|             Correct            |"
 	print "|                                |"
 	print "|________________________________|"
 
-# Reaction to the incorrect order
-def incorrect():
+def incorrect(call_a, call_b):
+    """
+    Informs the user that the order is incorrect and provides the call numbers
+    so the user can do a manual check.
+    
+    :param call_a:  first call number
+    
+    :param call_b:  second call number
+    """
+    
 	print "_________________________________"
 	print "|                                |"
 	print "|             Incorrect          |"
 	print "|                                |"
-	print "|________________________________|"	
-	
-# Gets barcodes, converts to call numbers, and determines if the call numbers
-# are in the correct order. Calls the correct() and incorrect() methods 
-# accordingly. If correct, it gets another barcode.
+	print "|________________________________|"
+    print call_a
+    print call_b
+    print
+    
 def main():
-	call_a = break_call_number(get_call_number())
-	call_b = break_call_number(get_call_number())
-	while is_correct(call_a, call_b):
-		correct()
-		call_a = call_b
-		call_b = break_call_number(get_call_number())
-	incorrect()
+    """
+    Creates a dictionary of call numbers and barcodes, gets call numbers from
+    input barcodes and determines if the call numbers are in the correct order.
+    Calls the correct() and incorrect() methods accordingly. If correct, promts
+    the user for another barcode.
+    """
+    
+    call_number_lookup = create_dictionary(FILE_NAME)
+    call_a = break_call_number(get_call_number(call_number_lookup))
+    call_b = break_call_number(get_call_number(call_number_lookup))
+    
+    while is_correct(call_a, call_b):
+        correct(call_a, call_b)
+        call_a = call_b
+        call_b = break_call_number(get_call_number())
+    incorrect(call_a, call_b)
 	
 main()
