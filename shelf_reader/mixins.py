c59d404e8eef4f1d10391cfcc147de32826c52b4
Austin <asmacdo@gmail.com>
2014-07-27 20:13:07 -0400

    add missed py3 compatibility files

---
+++ shelf_reader/mixins.py
@@ -0,0 +1,29 @@
+class ComparableMixin(object):
+    '''Implements rich operators for an object.
+    from python3porting.com'''
+
+    def _compare(self, other, method):
+        try:
+            return method(self._cmpkey(), other._cmpkey())
+        except (AttributeError, TypeError):
+            # _cmpkey not implemented, or return different type,
+            # so I can't compare with "other". Try the reverse comparison
+            return NotImplemented
+
+    def __lt__(self, other):
+        return self._compare(other, lambda s, o: s < o)
+
+    def __le__(self, other):
+        return self._compare(other, lambda s, o: s <= o)
+
+    def __eq__(self, other):
+        return self._compare(other, lambda s, o: s == o)
+
+    def __ge__(self, other):
+        return self._compare(other, lambda s, o: s >= o)
+
+    def __gt__(self, other):
+        return self._compare(other, lambda s, o: s > o)
+
+    def __ne__(self, other):
+        return self._compare(other, lambda s, o: s != o)
